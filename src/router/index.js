3db400ae57ae03f902839f9f665cf3f5446cbb8b
AlanMC7650 <alan.maldonado@ucb.edu.bo>
2025-04-30 22:44:17 -0400

    Agregado del documento explicativo del proyecto

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,8 @@ import Home from '../views/Home.vue'
 import People from '../views/People.vue'
 /*import Planets from '../views/Planets.vue'
 import Starships from '../views/Starships.vue'*/
-
+//se esperaba hacer una aplicacion que consuma people, planets y starship
+//pero se opto por solo consumir "People" de SWAPI
 const routes = [
   { path: '/', component: Home },
   { path: '/people', component: People },
